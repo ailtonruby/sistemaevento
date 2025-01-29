d647d7fc38dc970f80f2649e59181a082830ae7f
ailtonruby <143217022+ailtonruby@users.noreply.github.com>
2025-01-28 21:10:22 -0300

    atividade

---
+++ src/main/java/com/example/demo/entities/Categoria.java
@@ -0,0 +1,49 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_categoria")
+
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String descricao;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Atividade> atividade = new ArrayList();
+
+    public List<Atividade> getAtividade() {
+        return this.atividade;
+    }
+
+    public void setAtividade(List<Atividade> atividade) {
+        this.atividade = atividade;
+    }
+
+    public Categoria(int id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
---
+++ src/main/java/com/example/demo/entities/Participantes.java
@@ -0,0 +1,60 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_participantes")
+
+public class Participantes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    @Column(unique = true)
+    private String email;
+
+    @ManyToMany(mappedBy = "participantes")
+    private Set<Atividade> atividades = new HashSet<>();
+
+    public Participantes(String nome, int id, String email) {
+        this.nome = nome;
+        this.id = id;
+        this.email = email;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Atividade> getAtividades() {
+        return atividades;
+    }
+
+    public void setAtividades(Set<Atividade> atividades) {
+        this.atividades = atividades;
+    }
+}
